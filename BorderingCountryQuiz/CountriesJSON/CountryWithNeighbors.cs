cd9da5c7dbe2d0dd9b70d1cbce51974b3c3159a1
GET Academy <terje@getacademy.no>
2024-12-08 18:24:12 +0100

    Initial commit

---
+++ BorderingCountryQuiz/CountriesJSON/CountryWithNeighbors.cs
@@ -0,0 +1,15 @@
+﻿namespace BorderingCountryQuiz.CountriesJSON
+{
+    public class CountryWithNeighbors
+    {
+        public string country { get; set; }
+        public Neighbor[] neighbors { get; set; }
+    }
+
+    public class Neighbor
+    {
+        public string country { get; set; }
+        public string borderType { get; set; }
+    }
+
+}
---
+++ BorderingCountryQuiz/Program.cs
@@ -0,0 +1,7 @@
+﻿using System.Text.Json;
+using BorderingCountryQuiz.CountriesJSON;
+
+var json = File.ReadAllText("CountriesJSON/countries.json");
+var countries = JsonSerializer.Deserialize<CountryWithNeighbors[]>(json);
+
+Console.WriteLine();
